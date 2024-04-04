862c4315253a3e45dcf49d914d01e4f16fb26c6a
Enzo Miletta <milettaaa@gmail.com>
2024-04-04 20:50:22 -0300

    5º classroom
    yeeeeeeeeeee

---
+++ Aula 05 Web Development/index.js
@@ -0,0 +1,14 @@
+/*alert("Hello World")
+prompt("Digite seu nome")
+confirm("|Deseja sair?|")
+var nome = prompt("Digite seu nome")
+var idade = 23
+//console.log("Olá " + nome + ", Seja bem vindo")
+console.log(`Olá ${nome}, seja bem vindo!`)
+*/
+
+var nome = prompt("Digite seu nome:")
+var idade = prompt("Digite sua idade:")
+var profissao = prompt("Digite sua profissão")
+
+console.log(`Olá ${nome}, você possui ${idade} anos, e trabalha como ${profissao}. Seja bem vindo!`)
