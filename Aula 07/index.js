f2f0188bb185fab856251b1ae7898cbd523ee6d0
Enzo Miletta <milettaaa@gmail.com>
2024-04-18 21:48:38 -0300

    7º Classroom
    :)

---
+++ Aula 07/index.js
@@ -0,0 +1,7 @@
+function calcular(){
+    var livros = document.getElementById('numBooks').value;
+    var resultado = document.getElementById('resultado');
+
+    resultado.innerText = `O preço é ${soma}`;
+
+}
